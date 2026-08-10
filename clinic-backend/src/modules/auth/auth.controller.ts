d9389cbd069968dc9f5263874069867bde4548d2
htein linn <exercise999123@example.com>
2026-08-10 07:41:41 +0630

    added the js extensionf for ES module

--- clinic-backend/src/modules/auth/auth.controller.ts
+++ clinic-backend/src/modules/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import { prisma } from '../../config/prisma';
+import { prisma } from '../../config/prisma.js';
 import Role from '@prisma/client';
 
 const JWT_SECRET = process.env.JWT_SECRET || 'super-secret-key';
